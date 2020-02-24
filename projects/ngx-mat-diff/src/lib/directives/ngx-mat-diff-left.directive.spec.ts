d3600c9e55b390d3acc397978bff3198ba5193d1
jeremias98 <ramirezjeremias98@gmail.com>
2020-02-24 14:17:36 -0300

    Tests para la directiva

--- projects/ngx-mat-diff/src/lib/directives/ngx-mat-diff-left.directive.spec.ts
+++ projects/ngx-mat-diff/src/lib/directives/ngx-mat-diff-left.directive.spec.ts
@@ -5,39 +5,100 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 @Component({
-  template: `<textarea ngxMatDiffLeft></textarea><div class='result'>{{diffService.getLeftText()}}</div>`
+  template: `<textarea ngxMatDiffLeft></textarea>`
 })
-class TestComponent { constructor(public diffService: NgxMatDiffService) { } }
+class TestTextAreaComponent { constructor(public diffService: NgxMatDiffService) { } }
 
-describe('NgxMatDiffLeftDirective', () => {
+describe('NgxMatDiffLeftDirective using Textarea', () => {
 
-  let fixture: ComponentFixture<TestComponent>;
-  let component: TestComponent;
+  let fixtureTextArea: ComponentFixture<TestTextAreaComponent>;
+  let componentTextArea: TestTextAreaComponent;
+
   let inputEl: DebugElement;
-  let resultEl: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TestComponent, NgxMatDiffLeftDirective],
+      declarations: [TestTextAreaComponent, NgxMatDiffLeftDirective],
       providers: [NgxMatDiffService]
     });
-    fixture = TestBed.createComponent(TestComponent);
-    component = fixture.componentInstance;
-    inputEl = fixture.debugElement.query(By.css('textarea'));
-    resultEl = fixture.debugElement.query(By.css('.result'));
+    fixtureTextArea = TestBed.createComponent(TestTextAreaComponent);
+    componentTextArea = fixtureTextArea.componentInstance;
+
+    // fixtureDivComponent = TestBed.createComponent(TestDivComponent);
+    // componentDiv = fixtureTextArea.componentInstance;
   });
 
   it('Should create an instance', () => {
-    const directiveEl = fixture.debugElement.query(By.directive(NgxMatDiffLeftDirective));
+    const directiveEl = fixtureTextArea.debugElement.query(By.directive(NgxMatDiffLeftDirective));
     expect(directiveEl).toBeTruthy();
   });
 
-  it('should get value from textarea', () => {
-    const directiveEl = fixture.debugElement.query(By.directive(NgxMatDiffLeftDirective));
+  it('Should get value from textarea', () => {
+    const directiveEl = fixtureTextArea.debugElement.query(By.directive(NgxMatDiffLeftDirective));
     const directiveInstance: NgxMatDiffLeftDirective = directiveEl.injector.get(NgxMatDiffLeftDirective);
 
-    inputEl.nativeElement.value = 'Holis';
-    expect(directiveInstance.getValue()).toBe('Holis');
+    inputEl = fixtureTextArea.debugElement.query(By.css('textarea'));
+
+    inputEl.nativeElement.value = 'Expected value';
+    expect(directiveInstance.getValue()).toBe('Expected value');
 
   });
+
+  // it('Should get value on keyup', () => {
+  //   const directiveEl = fixtureTextArea.debugElement.query(By.directive(NgxMatDiffLeftDirective));
+  //   const directiveInstance: NgxMatDiffLeftDirective = directiveEl.injector.get(NgxMatDiffLeftDirective);
+
+  //   inputEl = fixtureTextArea.debugElement.query(By.css('textarea'));
+  //   inputEl.triggerEventHandler('keyup', null);
+  //   fixtureTextArea.detectChanges();
+
+  //   spyOn(directiveInstance, "onKeyUp");
+
+  //   inputEl = fixtureTextArea.debugElement.query(By.css('textarea'));
+
+  //   inputEl.nativeElement.value = 'Expected value';
+  //   expect(directiveInstance.onKeyUp).toHaveBeenCalled();
+
+  // });
+
+});
+
+@Component({
+  template: `<div ngxMatDiffLeft></div>`
+})
+class TestDivComponent { constructor(public diffService: NgxMatDiffService) { } }
+
+describe('NgxMatDiffLeftDirective using innerText', () => {
+
+  let fixtureDivComponent: ComponentFixture<TestDivComponent>;
+  let componentDiv: TestDivComponent;
+
+  let inputEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestDivComponent, NgxMatDiffLeftDirective],
+      providers: [NgxMatDiffService]
+    });
+
+    fixtureDivComponent = TestBed.createComponent(TestDivComponent);
+    componentDiv = fixtureDivComponent.componentInstance;
+  });
+
+  it('Should create an instance', () => {
+    const directiveEl = fixtureDivComponent.debugElement.query(By.directive(NgxMatDiffLeftDirective));
+    expect(directiveEl).toBeTruthy();
+  });
+
+  it('Should get inner text from div', () => {
+    const directiveEl = fixtureDivComponent.debugElement.query(By.directive(NgxMatDiffLeftDirective));
+    const directiveInstance: NgxMatDiffLeftDirective = directiveEl.injector.get(NgxMatDiffLeftDirective);
+
+    inputEl = fixtureDivComponent.debugElement.query(By.css('div'));
+
+    inputEl.nativeElement.value = 'Expected inner text';
+    expect(directiveInstance.getValue()).toBe('Expected inner text');
+
+  });
+
 });
